46bdedfc4d0f00351d703406639d66694bd59eb5
Alfredo C Navarro Jr <anavarro@hyperglance.com>
2025-09-30 20:41:16 +0800

    Initialize springboot project

---
+++ src/main/java/org/job/sbjobex/SbJobExApplication.java
@@ -0,0 +1,13 @@
+package org.job.sbjobex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJobExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbJobExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/job/sbjobex/SbJobExApplicationTests.java
@@ -0,0 +1,13 @@
+package org.job.sbjobex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbJobExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
